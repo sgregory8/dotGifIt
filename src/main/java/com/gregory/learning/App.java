2ee0b55a82c2b2908eb2fba855918c6034e24d4b
Gregory, Sam (ELS-OXF) <s.gregory@elsevier.com>
2019-11-30 20:46:01 +0000

    initial generation

---
+++ src/main/java/com/gregory/learning/App.java
@@ -0,0 +1,17 @@
+package com.gregory.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      String str = "hello1";
+      String str2 = str;
+      str = "goodbye";
+
+      System.out.println(str2);
+    }
+}
