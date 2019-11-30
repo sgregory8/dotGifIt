0af00da55339ee9cac547fc0e6b29de8faf322dc
Gregory, Sam (ELS-OXF) <s.gregory@elsevier.com>
2019-11-30 22:38:49 +0000

    writes images to source

--- src/main/java/com/gregory/learning/App.java
+++ src/main/java/com/gregory/learning/App.java
@@ -1,17 +1,41 @@
 package com.gregory.learning;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-      String str = "hello1";
-      String str2 = str;
-      str = "goodbye";
+public class App {
 
-      System.out.println(str2);
+  public static void main(String[] args) throws AWTException, IOException {
+
+    List<BufferedImage> imageList = new ArrayList<>();
+
+    Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+    Robot robot = new Robot();
+
+    System.out.println("getting screen images...");
+
+    int count = 0;
+
+    while (count < 10) {
+
+      BufferedImage image = robot.createScreenCapture(screenRect);
+      imageList.add(image);
+      ImageIO.write(image, "jpg",
+          new File("/users/gregory1/learning/java-projects/dot-gif-it/images/" + count + ".jpg"));
+
+      count++;
+
     }
+  }
 }
